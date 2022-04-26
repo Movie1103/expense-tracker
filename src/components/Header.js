f9614b58b14ae57e6490cf6a814653df1196bde9
Movie1103 <tpawarut@gmail.com>
2022-04-26 15:54:24 +0700

    completed

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <nav className="navbar navbar-expand-sm sticky-top navbar-light bg-warning">
+      <div className="container-fluid">
+        <a className="navbar-brand text-black-50 fw-bolder" href="/">
+          EXPENSE TRACKER
+        </a>
+
+        <div className="navbar-collapse">
+          
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Header
---
+++ src/components/TransactionForm.js
@@ -0,0 +1,69 @@
+import React from 'react'
+
+function TransactionForm() {
+  return (
+    <div className="border bg-white rounded-2 p-3">
+      <form className="row g-3">
+        <div className="col-6">
+          <input 
+            type="radio" 
+            className="btn-check"
+            id="cbx-expense"
+            name="type"
+            defaultChecked
+          />
+          <label 
+            className="btn btn-outline-danger rounded-0 rounded-start"
+            htmlFor="cbx-expense" 
+          >Expense</label>
+          <input 
+            type="radio" 
+            className="btn-check"
+            id="cbx-income"
+            name="type"
+          />
+          <label 
+            className="btn btn-outline-success rounded-0 rounded-end"
+            htmlFor="cbx-income" 
+          >Income</label>
+        </div>
+
+        <div className="col-6 d-flex justify-content-end">
+          <i className="fa-solid fa-xmark" role="button"></i>
+        </div>
+
+        <div className="col-sm-6">
+          <label className="form-label">Payee</label>
+          <input type="text" className="form-control" />
+        </div>
+
+        <div className="col-sm-6">
+          <label className="form-label">Category</label>
+          <select className="form-select">
+            <option>Food</option>
+            <option>Transport</option>
+          </select>
+        </div>
+
+        <div className="col-sm-6">
+          <label className="form-label">Amount</label>
+          <input type="text" className="form-control" />
+        </div>
+
+        <div className="col-sm-6">
+          <label className="form-label">Date</label>
+          <input type="date" className="form-control" />
+        </div>
+
+        <div className="col-12">
+          <div className="d-grid mt-3">
+            <button className="btn btn-primary">Save</button>
+          </div>
+        </div>
+
+      </form>
+    </div>
+  )
+}
+
+export default TransactionForm;
